95826895a49f87b54af1dcb092b262da5bc64cf5
haixiangyan <haixiang6123@gmail.com>
2021-04-25 14:21:13 +0800

    feat: 添加第一个测试

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -1,7 +1,40 @@
-import TestAgent from "../lib/agent";
+import express from 'express'
+import request from "../lib";
 
-describe('test', () => {
-  it('testAgent', () => {
-    expect(TestAgent()).toEqual('TestAgent')
-  })
-})
+describe('request(url)', function () {
+  it('should be supported', function (done) {
+    const app = express();
+    let s;
+
+    app.get('/', function (req, res) {
+      res.send('hello');
+    });
+
+    s = app.listen(function () {
+      const url = 'http://localhost:' + s.address().port;
+      request(url)
+        .get('/')
+        .expect('hello', done);
+    });
+  });
+
+  describe('.end(cb)', function () {
+    it('should set `this` to the test object when calling cb', function (done) {
+      const app = express();
+      let s;
+
+      app.get('/', function (req, res) {
+        res.send('hello');
+      });
+
+      s = app.listen(function () {
+        const url = 'http://localhost:' + s.address().port;
+        const test = request(url).get('/');
+        test.end(function (err, res) {
+          expect(this).toEqual(test)
+          done();
+        });
+      });
+    });
+  });
+});
